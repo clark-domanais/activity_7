634c6f42c16c1ed273440fda47492900bcd4ec37
Clark Domanais <clarkdomanais@gmail.com>
2026-06-04 01:11:17 +0800

    added a getter for the speed of the car

--- car_class.py
+++ car_class.py
@@ -8,8 +8,12 @@ class Car:
     def accelerate(self):
         self.__speed += 5
 
-    # Brake 
+    # Brake
     def brake(self):
         self.__speed -= 5
         if self.__speed < 0:
             self.__speed = 0
+
+    # Getter for speed
+    def get_speed(self):
+        return self.__speed
