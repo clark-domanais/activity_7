e1288e64ca44db8763cde2c286451c196c1df279
Clark Domanais <clarkdomanais@gmail.com>
2026-06-04 16:00:35 +0800

    setting the values for pet object

--- pet_class.py
+++ pet_class.py
@@ -31,3 +31,8 @@ my_pet = Pet()
 name = input("Enter your pet's name: ")
 animal_type = input("Enter the type of animal (Dog, Cat, Bird, etc.): ")
 age = int(input("Enter your pet's age: "))
+
+# Set values using mutators
+my_pet.set_name(name)
+my_pet.set_animal_type(animal_type)
+my_pet.set_age(age)
